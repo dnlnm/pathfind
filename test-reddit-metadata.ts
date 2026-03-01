59d046bd6aea11cf8b2cf2f0cc6d5934d0266314
dnlnm <danielnaim5543@gmail.com>
2026-03-01 21:43:49 +0800

    feat: Remove SQLite database files and update test and fetch scripts from git.

--- test-reddit-metadata.ts
+++
@@ -1,9 +0,0 @@
-import { fetchUrlMetadata } from "./src/lib/metadata-fetcher.js";
-
-async function run() {
-    const url = "https://www.reddit.com/r/jav/comments/1rhevbs/what_gel_or_cream_did_he_use/";
-    const data = await fetchUrlMetadata(url);
-    console.log(data);
-}
-
-run();
